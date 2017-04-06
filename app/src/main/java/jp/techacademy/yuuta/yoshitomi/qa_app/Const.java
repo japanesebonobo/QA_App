c4ead89df93f7bd9ee7cd953cec52c18442cc798
japanesebonobo <tomi119119@gmail.com>
2017-04-07 01:48:26 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuuta/yoshitomi/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yuuta.yoshitomi.qa_app;
+
+/**
+ * Created by yoshitomi on 2017/04/06.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users";
+    public static final String ContentsPATH = "contents";
+    public static final String AnswersPATH = "answers";
+
+    public static final String NameKEY = "name";
+}
